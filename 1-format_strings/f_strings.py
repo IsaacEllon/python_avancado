d92e41d2002d0252cf8aa348b2506f08bcddc4c6
Isaac Ellon <isaacellon@gmail.com>
2024-08-30 16:36:31 -0300

    1

---
+++ 1-format_strings/f_strings.py
@@ -0,0 +1,11 @@
+nome = "Isaac Ellon"
+altura = 1.74
+peso = 64
+
+imc = peso / altura** 2
+linha_1 = f"{nome} tem {altura:.2} de altura"  #f_string
+linha_2 = f"Pesa {peso} quilos e seu imc é:"
+linha_3 = f"{imc:.2f}"
+print(linha_1)
+print(linha_2)
+print(linha_3)
