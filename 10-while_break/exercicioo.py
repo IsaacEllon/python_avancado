8691a4aebc5e2f59d95a0849116796ba6b701521
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2024-10-11 13:20:16 -0300

    1

--- 10-while_break/exercicioo.py
+++ 10-while_break/exercicioo.py
@@ -2,14 +2,19 @@ frase = 'O python é uma linguagem de programação '\
     'multiparadigma' \
     'python foi criado por Guido Van Rossum'
 
-print(frase.count('python')) # count - verifica quantas vezes o caractere a aparece. 
+#print(frase.count('python')) # count - verifica quantas vezes o caractere a aparece. 
 
-i = 0 
+i = 0
 qtd_apareceu_mais_vezes = 0
 letra_apareceu_mais_vezes = ''
 
 while i < len(frase):
     letra_atual = frase[i]
+
+    if letra_atual == ' ':
+        i += 1
+        continue
+
     qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
 
     if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
@@ -18,8 +23,8 @@ while i < len(frase):
 
     i += 1
 
-    print(
-        'A letra que apareceu mais vezes foi '
-        f'{letra_apareceu_mais_vezes} que apareceu ' 
-        f'{qtd_apareceu_mais_vezes}x'
-    )
+print(
+    'A letra que apareceu mais vezes foi '
+    f'"{letra_apareceu_mais_vezes}" que apareceu '
+    f'{qtd_apareceu_mais_vezes}x'
+)
