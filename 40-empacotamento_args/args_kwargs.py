40738438692d6c11154377423a12bac8ac813bff
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2025-01-29 15:26:05 -0300

    1

--- 40-empacotamento_args/args_kwargs.py
+++ 40-empacotamento_args/args_kwargs.py
@@ -9,7 +9,40 @@ pessoa = { #dicionário
     'sobrenome': 'Souza',
 }
 
-a, b = pessoa.items()
-print(a, b)
+"""
+(a1, a2), (b1, b2) = pessoa.items()
+print(a1, a2)
+print(b1, b2)
+
+for chave, valor in pessoa.items():
+    print(chave, valor)
+"""
+
+dados_pessoa = {
+    'idade': 16,
+    'altura': 1.6,
+}
+
+pessoa_completa = {**pessoa, **dados_pessoa}
+
+print(pessoa_completa)
 
 # kwargs - keyword arguments (argumentos nomeados)
+
+def mostro_argumentos_nomeados(*args, **kwargs):
+    print('Não nomeados:', args)
+    
+    for chave, valor in kwargs.items():
+        print(chave, valor)
+    
+#mostro_argumentos_nomeados(nome='joana', qlq=123)
+#mostro_argumentos_nomeados(**pessoa_completa)
+
+configuracoes = {
+    'arg1': 1,
+    'arg2': 2,
+    'arg3': 3,
+    'arg4': 4,
+}
+
+mostro_argumentos_nomeados(**configuracoes)
